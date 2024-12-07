5734cc96a619ba241dd33b13a93f500399fedbc1
1nchy <wangyinqi23@mails.ucas.ac.cn>
2024-12-07 10:08:46 +0800

    feat(cmake): link lib from third/

--- cmake/Findthird_lib.cmake
+++ cmake/Findthird_lib.cmake
@@ -2,7 +2,7 @@ set(<third_lib>_FOUND TRUE)
 set(<third_lib>_ROOT_DIR ${CMAKE_SOURCE_DIR}/third/<third_lib>)
 
 find_path(<third_lib>_INCLUDE_DIR NAMES <third_lib>.hpp PATHS ${<third_lib>_ROOT_DIR}/include)
-find_library(<third_lib>_LIBRARY NAMES lib<third_lib>.a PATHS ${CMAKE_SOURCE_DIR}/lib)
+find_library(<third_lib>_LIBRARY NAMES lib<third_lib>.a PATHS ${<third_lib>_ROOT_DIR}/lib)
 
 set(<third_lib>_INCLUDE_DIR ${<third_lib>_INCLUDE_DIR})
 set(<third_lib>_LIBRARIES ${<third_lib>_LIBRARY})
